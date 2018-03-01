67fb35b02575a10976fc2cdaca879d60ec5cb629
davidbinstock <binstock.d@gmail.com>
2018-02-28 22:45:59 -0600

    somewhere in the middle

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,52 @@
 //choose a random word (maybe use array of predetermined words...)
 //have document dispay number of blanks as letters in word 
 
-//var wordLength = 4;
+var userGuess = "d";
+var currentWord = "souse";
 var wordDisplay = document.getElementById("word-display");
+var solvedCounter = 0;
+var guessesLeft = 10;
+var guessedArray = [];
+var guessedRight = false;
 
 //wordDisplay.innerHTML = '<div class="letter-holder">hello</div>';
 
-
-wordDisplay.innerHTML = createDisplay(5);
-
+wordDisplay.innerHTML = createDisplay(currentWord.length);
 
 
+for(var i=0; i < currentWord.length; i++){
+    if(userGuess == currentWord[i]){
+        console.log("in position " + i);
+        document.getElementById('position-'+(i+1)).innerHTML = currentWord[i];
+        solvedCounter++;
+        console.log("solved letters: " + solvedCounter);
+        guessedRight = true;
+
+    } else {
+        console.log("NOT in position " + i);
+    }
+}
+
+if(guessedRight == false){
+    guessesLeft--;
+}
+guessedArray.push(userGuess);
+
+console.log("guesses left: " + guessesLeft);
+console.log("letters guessed: " + guessedArray);
+
+
+document.getElementById('test').innerHTML = currentWord[0]+currentWord[1]+currentWord[2]+currentWord[3]+currentWord[4]; 
+
+
+
+// this function generates blanks spaces for each letter in the word
 function createDisplay(wordLength){
     // declare empty string variable
     var displayContent = "";
     // for each letter in the word, add a new <div> element to the HTML with a "_"
-    for (var i=0; i < wordLength; i++){
-        displayContent = displayContent+'<div class="letter-holder">_</div>';
+    for (var i=1; i < wordLength+1; i++){
+        displayContent = displayContent+'<div class="letter-holder position-" id="position-' + i + '">_</div>';
     }
     return displayContent;
 
