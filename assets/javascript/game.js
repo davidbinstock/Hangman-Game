8fb93361b3b94303203d8779e9c5438d8c89e40b
davidbinstock <binstock.d@gmail.com>
2018-03-01 00:30:29 -0600

    another place in the middle

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,19 +8,43 @@ var solvedCounter = 0;
 var guessesLeft = 10;
 var guessedArray = [];
 var guessedRight = false;
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 //generate the <div>s for the word
 wordDisplay.innerHTML = createDisplay(currentWord.length);
 
 //guessing letters
-document.onkeyup = function(event){
+//start of on-key-up function ---------------------------------------------------------
+document.onkeyup = function(event){ 
+    //pass the letter pressed (as a string) to userGuess variable
     userGuess = event.key
+    console.log("You typed: " + userGuess)
+        
+    //check if the key is a letter, otherwise tell them to pick a letter
+    if(alphabet.indexOf(userGuess) < 0){
+        console.log("not a letter")
+        document.getElementById('message-text').innerHTML = "That's not a letter"
+        return;
+    }
+
+    //check if the letter has been used before if so, ask them to pick a new letter
+    if(guessedArray.indexOf(userGuess) >= 0){
+        console.log("you already guessed that letter");
+        document.getElementById('message-text').innerHTML = "You already guessed that letter"
+        return;
+    }
+
+     
+    
+    //reset the guessedRight value so that loop can properly moniter if this guess was correct
     guessedRight = false;
 
-    console.log("You Guessed: " + userGuess)
+    //for each letter in the word, check if the guessed letter matches
     for(var i=0; i < currentWord.length; i++){
+        //if the letter matches....
         if(userGuess == currentWord[i]){
             console.log("in position " + i);
+            // replace the 
             document.getElementById('position-'+(i+1)).innerHTML = currentWord[i];
             solvedCounter++;
             console.log("solved letters: " + solvedCounter);
@@ -30,10 +54,14 @@ document.onkeyup = function(event){
             console.log("NOT in position " + i);
         }
     }
-
+    //if after the for loop, the value of guessedRight is still false, that means that the guess was not correct and guesses left should decrease by one
     if(guessedRight == false){
         guessesLeft--;
+        document.getElementById('message-text').innerHTML = "You just guessed: " + userGuess + ". WRONG!!"
+    } else {
+        document.getElementById('message-text').innerHTML = "You just guessed: " + userGuess + ". CORRECT!!"
     }
+
     guessedArray.push(userGuess);
 
     console.log("guesses left: " + guessesLeft);
@@ -42,14 +70,18 @@ document.onkeyup = function(event){
     document.getElementById("guesses-left").innerHTML = guessesLeft;
     document.getElementById("guessed-letters").innerHTML = guessedArray;
 
+    // if the solvedCounter = the length of the word (i.e. all letters have been guessed right) then wins should increase and the game should resatart
+
+    //if the guesses is zero, then you lose the game... give option to start over...
 
 
-}
+
+} //end of on-key-up function ----------------------------------------------------------------
 
 
 document.getElementById('test').innerHTML = currentWord[0]+currentWord[1]+currentWord[2]+currentWord[3]+currentWord[4]; 
 
-
+// Create function to generate new word
 
 // this function generates blanks spaces for each letter in the word
 function createDisplay(wordLength){
