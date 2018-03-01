dbdf449382f77a801f45be08b9e38eaf25279fea
davidbinstock <binstock.d@gmail.com>
2018-02-28 23:40:15 -0600

    somewhere else in the middle

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,50 @@
 //choose a random word (maybe use array of predetermined words...)
 //have document dispay number of blanks as letters in word 
 
-var userGuess = "d";
-var currentWord = "souse";
+var userGuess = "";
+var currentWord = "minister";
 var wordDisplay = document.getElementById("word-display");
 var solvedCounter = 0;
 var guessesLeft = 10;
 var guessedArray = [];
 var guessedRight = false;
 
-//wordDisplay.innerHTML = '<div class="letter-holder">hello</div>';
-
+//generate the <div>s for the word
 wordDisplay.innerHTML = createDisplay(currentWord.length);
 
+//guessing letters
+document.onkeyup = function(event){
+    userGuess = event.key
+    guessedRight = false;
 
-for(var i=0; i < currentWord.length; i++){
-    if(userGuess == currentWord[i]){
-        console.log("in position " + i);
-        document.getElementById('position-'+(i+1)).innerHTML = currentWord[i];
-        solvedCounter++;
-        console.log("solved letters: " + solvedCounter);
-        guessedRight = true;
+    console.log("You Guessed: " + userGuess)
+    for(var i=0; i < currentWord.length; i++){
+        if(userGuess == currentWord[i]){
+            console.log("in position " + i);
+            document.getElementById('position-'+(i+1)).innerHTML = currentWord[i];
+            solvedCounter++;
+            console.log("solved letters: " + solvedCounter);
+            guessedRight = true;
 
-    } else {
-        console.log("NOT in position " + i);
+        } else {
+            console.log("NOT in position " + i);
+        }
     }
-}
 
-if(guessedRight == false){
-    guessesLeft--;
-}
-guessedArray.push(userGuess);
+    if(guessedRight == false){
+        guessesLeft--;
+    }
+    guessedArray.push(userGuess);
 
-console.log("guesses left: " + guessesLeft);
-console.log("letters guessed: " + guessedArray);
+    console.log("guesses left: " + guessesLeft);
+    console.log("letters guessed: " + guessedArray);
+
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+    document.getElementById("guessed-letters").innerHTML = guessedArray;
+
+
+
+}
 
 
 document.getElementById('test').innerHTML = currentWord[0]+currentWord[1]+currentWord[2]+currentWord[3]+currentWord[4]; 
